875611284a66b4d294e18679fb555404c9b7d509
Karthik Vijayraghavan <karthik1024@gmail.com>
2022-09-18 19:31:37 -0700

    Importing local packages

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,7 +3,7 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hellp(name string) string {
+func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"example/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	message := greetings.Hello("NameA")
+	fmt.Println(message)
 }
