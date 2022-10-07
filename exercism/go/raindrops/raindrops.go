9f8fd4dbce380094ed974a1ef57d174c3297a6cc
Karthik Vijayraghavan <karthik1024@gmail.com>
2022-10-06 22:00:19 -0700

    More efficient raindrop solution

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -3,27 +3,21 @@ package raindrops
 import "strconv"
 
 func Convert(number int) string {
-	isFac := false
 	s := ""
 	if number%3 == 0 {
 		s += "Pling"
-		isFac = true
 	}
 
 	if number%5 == 0 {
 		s += "Plang"
-		isFac = true
 	}
 
 	if number%7 == 0 {
 		s += "Plong"
-		isFac = true
 	}
 
-	if !isFac {
+	if s == "" {
 		return strconv.Itoa(number)
-	} else {
-		return s
 	}
-
+	return s
 }
