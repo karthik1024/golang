a91ce5b275efd13173fb0eef62bc5967cdf6f1a5
Karthik Vijayraghavan <karthik1024@gmail.com>
2022-10-09 09:52:32 -0700

    Update isogram based on feedback

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,7 +1,6 @@
 package isogram
 
 import (
-	"strings"
 	"unicode"
 )
 
@@ -9,19 +8,18 @@ var exists = struct{}{}
 
 // IsIsogram returns True when a word is an isogram and False otherwise.
 func IsIsogram(word string) bool {
-	// mimic a set with a 0 byte type as value.
-	s := make(map[rune]struct{})
+	m := make(map[rune]bool)
 
 	for _, c := range word {
-		c = unicode.ToLower(c)
-		if strings.ContainsRune(" -", c) {
+		if !unicode.IsLetter(c) {
 			continue
 		}
 
-		if _, ok := s[c]; ok {
+		c = unicode.ToLower(c)
+		if m[c] {
 			return false
 		}
-		s[c] = exists
+		m[c] = true
 	}
 	return true
 }
